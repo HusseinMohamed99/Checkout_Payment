de8f67456789c451d88de8550e82b91351099168
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-11-21 21:26:28 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckoutPayment());
+}
+
+class CheckoutPayment extends StatelessWidget {
+  const CheckoutPayment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
