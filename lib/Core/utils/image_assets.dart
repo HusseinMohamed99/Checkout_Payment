7671dd66566f0b620f3dd2eda7ef0c9acfbbbcba
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-12-01 18:33:15 +0200

    - added images - added splash screen

---
+++ lib/Core/utils/image_assets.dart
@@ -0,0 +1,11 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesPaypal
+  /// assets/images/paypal.png
+  static const String imagesPaypal = "assets/images/paypal.png";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+}
