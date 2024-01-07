01610e0fbade2d5a8721bb889772fabe46c2bfa0
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-01-07 14:18:38 +0200

    Add PayPal payment integration

--- lib/Features/cart/ui/widgets/custom_button_bloc_consumer.dart
+++ lib/Features/cart/ui/widgets/custom_button_bloc_consumer.dart
@@ -1,11 +1,13 @@
+import 'dart:developer';
+
 import 'package:checkout_payment/Core/helpers/extensions.dart';
 import 'package:checkout_payment/Core/routing/routes.dart';
 import 'package:checkout_payment/Core/widgets/custom_button.dart';
-import 'package:checkout_payment/Features/cart/data/model/payment_intent_input/payment_intent_input_model.dart';
 import 'package:checkout_payment/Features/cart/logic/cubit/stripe_payment_cubit.dart';
 import 'package:checkout_payment/Features/cart/logic/cubit/stripe_payment_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 
 class CustomButtonBlocConsumer extends StatelessWidget {
   const CustomButtonBlocConsumer({
@@ -32,14 +34,70 @@ class CustomButtonBlocConsumer extends StatelessWidget {
           text: 'Continue',
           isLoading: state is StripePaymentLoading ? true : false,
           onTap: () {
-            PaymentIntentInputModel paymentIntentInputModel =
-                PaymentIntentInputModel(
-              amount: '100',
-              currency: 'USD',
-              customerId: 'cus_PKLCkhYOMWEOTS',
-            );
-            BlocProvider.of<StripePaymentCubit>(context).makePayment(
-              paymentIntentInputModel: paymentIntentInputModel,
+            // PaymentIntentInputModel paymentIntentInputModel =
+            //     PaymentIntentInputModel(
+            //   amount: '100',
+            //   currency: 'USD',
+            //   customerId: 'cus_PKLCkhYOMWEOTS',
+            // );
+            // BlocProvider.of<StripePaymentCubit>(context).makePayment(
+            //   paymentIntentInputModel: paymentIntentInputModel,
+            // );
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => PaypalCheckoutView(
+                  sandboxMode: true,
+                  clientId: "YOUR CLIENT ID",
+                  secretKey: "YOUR SECRET KEY",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": "100",
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": "100",
+                          "shipping": "0",
+                          "shipping_discount": 0
+                        }
+                      },
+                      "description": "The payment transaction description.",
+                      // "payment_options": {
+                      //   "allowed_payment_method":
+                      //       "INSTANT_FUNDING_SOURCE"
+                      // },
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": "10",
+                            "currency": "USD"
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": "12",
+                            "currency": "USD"
+                          }
+                        ],
+                      }
+                    }
+                  ],
+                  note: "Contact us for any questions on your order.",
+                  onSuccess: (Map params) async {
+                    log("onSuccess: $params");
+                    Navigator.pop(context);
+                  },
+                  onError: (error) {
+                    log("onError: $error");
+                    Navigator.pop(context);
+                  },
+                  onCancel: () {
+                    print('cancelled:');
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
             );
           },
         );
